3623abc71e0aa5885863123ab5c267e4ea4fe0a6
Mateusz Dobrowolny <mateusz.dobrowolny@gmail.com>
2015-08-18 11:38:13 +0200

    'v1'

--- runme.bat
+++ runme.bat
@@ -14,7 +14,17 @@ git clone https://github.com/mateusz-dobrowolny/CItest-res
 
 REM Create file with paths for the spoon.me files:
 cd spoonme
-FOR /F "tokens=* " %%i IN (%CItest%\CItest-res\JenkinsCI.txt) DO %%i
+FOR /F "tokens=* " %%i IN (%CItest%\CItest-res\JenkinsCI.txt) DO cmd /c %%i
 
-REM spoon logujesz siê na konto testbuild
-REM spoon build --overwrite spoon.me
+REM login to spoon
+spoon login CItest C1t3stp4s5
+
+REM Loop through all the spoon.me files, build the app and check the result
+FOR /F %%i IN (spoonme-files.txt) do (
+spoon build --no-base --overwrite %%i
+
+REM TODO: Catch the result of 'spoon build' command.
+REM echo Exit code for %%i is %ERRORLEVEL% > test-results.txt 
+)
+
+spoon images > images-created.txt
