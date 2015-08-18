982e21062f942b98ac24d9d411c834895b0a3cab
Mateusz Dobrowolny <mateusz.dobrowolny@gmail.com>
2015-08-18 04:30:35 +0200

    ok

--- runme.bat
+++ runme.bat
@@ -1,22 +1,20 @@
 @echo off
 
 REM Define working folder where the scripts will be copied
-set CItest="C:\CItest\"
+set CItest=C:\CItest
 
 REM Prepare working folder with the content
 rmdir %CItest% /s /q
 mkdir %CItest%
-cd %CItest%
+cd /d %CItest%
 
-REM If git is installed locally, just copy files from the GitHub
-git clone https://github.com/apoonapps/spoonme
+REM If git is installed locally, just copy files from GitHub
+git clone https://github.com/spoonapps/spoonme
 git clone https://github.com/mateusz-dobrowolny/CItest-res
 
-REM cd CItest
-
 REM Create file with paths for the spoon.me files:
-FOR /F "tokens= delims= " %%i IN (%CItest%\CItest-res\JenkinsCI.txt) DO
-%%i
+cd spoonme
+FOR /F "tokens=* " %%i IN (%CItest%\CItest-res\JenkinsCI.txt) DO %%i
 
 REM dla ka¿dej aplikacji z pliku txt wykonaj:  launch.bat`
 
